fbaff76e1a4c1f1dd38c1d93acd6f8b7646e4bd3
Dewei Liu <deweiliu@hotmail.com>
2020-02-07 17:12:43 +0000

    Parse html to text

---
+++ repeater/views/repeat/parser.py
@@ -0,0 +1,15 @@
+import requests
+import html2text
+def parse_webpage(url):
+    webpage=dict()
+
+
+    response=requests.get(url)
+    body=response.text
+    converter = html2text.HTML2Text()
+    converter.ignore_links = False
+    text = converter.handle(body)
+
+    webpage['title']='Web title'
+    webpage['content']=text
+    return webpage
--- repeater/views/repeat/repeat.py
+++ repeater/views/repeat/repeat.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from repeater.views.repeat import decoder
+from repeater.views.repeat import parser
 def repeat(request,encoded_url):
    
     url=decoder.decode(encoded_url)
-    return HttpResponse("Hello, world. You are visiting %s"%url)
+    webpage=parser.parse_webpage(url)
+
+    return HttpResponse(webpage['content'])
