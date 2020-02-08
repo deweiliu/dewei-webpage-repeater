30455501357c12c0f897243b8ccb9384418319d7
Dewei Liu <deweiliu@hotmail.com>
2020-02-08 01:37:44 +0000

    change the hyper link to the application site

--- src/repeater/views/repeat/parser.py
+++ src/repeater/views/repeat/parser.py
@@ -1,16 +1,40 @@
 from bs4 import BeautifulSoup
+from urllib import parse
+
 
 class Parser():
-    def __init__(self,response):
+    def __init__(self, response):
         super().__init__()
-        soup = BeautifulSoup(response.text, "html.parser") #Parse HTML Page
-        self.title=soup.title.string
-        self.body = soup.find('body')
-        
-             
+
+        soup = BeautifulSoup(response.text, "html.parser")  # Parse HTML Page
+        self.title = soup.title.string
+        body = soup.find('body')
+        for a in soup.findAll('a'):
+            try:
+                link = a['href']
+                new_link = self.change_link(response.url, link)
+                a['href'] = new_link
+            except:
+                pass
+
+        self.body = body
+
+    def change_link(self, url, link):
+
+        if(link.startswith('/')):
+            # change the relative link to obsulate link of the origin domain
+            p = parse.urlparse(url)
+            link = p.scheme+"://"+p.netloc+link
+
+        # URL encoding
+        link = parse.quote(link, safe='')
+        # change to relative link to this app
+        link = "/?url="+link
+        print(link)
+        return link
 
     def get_body(self):
         return self.body
+
     def get_title(self):
         return self.title
-        
