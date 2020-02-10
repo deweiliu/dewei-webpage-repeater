408fe42f72fb8f4bfaecc434dc01393d8fb53341
Dewei Liu <deweiliu@hotmail.com>
2020-02-10 00:15:45 +0000

    fix issue

--- src/repeater/views/get_time.py
+++ src/repeater/views/get_time.py
@@ -3,9 +3,12 @@ from datetime import datetime
 
 
 def value():
-    return str(time.time())
+    return str(time.gmtime())
 
-def string(time_value=time.gmtime()):
+
+def string(time_value=None):
+    if(time_value == None):
+        time_value = value()
     time_value = int(float(time_value))
     time_object = datetime.fromtimestamp(time_value)
     return time_object.strftime("%A %d %B %Y %H:%M:%S GMT")
